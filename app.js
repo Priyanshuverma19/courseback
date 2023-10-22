671f3905a6866b828bc394d6cf80fcc7fca671ac
priyanshu verma <priyanshubalram19112001@gmail.com>
2023-10-22 12:11:08 +0530

    cradintials change

--- app.js
+++ app.js
@@ -19,7 +19,7 @@ app.use(
 app.use(cookieParser());
 app.use(cors({
     origin: process.env.FRONTEND_URL,
-    Credentials:true,
+    credentials:true,
     methods:["GET","POST","PUT","DELETE"]
 }
 ));
